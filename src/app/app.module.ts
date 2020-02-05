2958cc5faa447f362d7b3a632ccb8537d0612b2b
vaibhav sharma <vsharma20v@gmail.com>
2020-02-05 01:52:53 -0500

    add general header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContainerComponent } from './container/container.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContainerComponent
+    ContainerComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule
